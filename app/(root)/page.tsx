6bf7c143cd2b47b57ba3d971ccfb3eb1fff111f7
cezene <cezenesn@hotmail.com.br>
2025-04-25 17:52:49 -0300

    added chartjs and react counter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
   const loggedIn = { firstName: "Luna" };
@@ -13,6 +14,11 @@ const Home = () => {
             title="Welcome"
             user={loggedIn.firstName || "Uncle Scoorage"}
             subtext="Access to manage your account and trasactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
       </div>
