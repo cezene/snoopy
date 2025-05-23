1c53e918ec1aa1f3647bfd7cf496b6bec2fb2240
cezene <cezenesn@hotmail.com.br>
2025-05-23 15:59:51 -0300

    implementing sentry

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,9 +8,14 @@ export default function RootLayout({
   return (
     <main className="flex min-h-screen w-full justify-between font-inter bg-gradient-to-b from-gray-900 to-lime-800">
       <div className="auth-asset">
-        <div> 
-
-        <Image src="/icons/auth-image-bank-snoopy.png" className="rounded-xl !ml-10" alt="Snoopy Image" width={700} height={800}/>
+        <div>
+          <Image
+            src="/icons/auth-image-bank-snoopy.png"
+            className="rounded-xl !ml-10"
+            alt="Snoopy Image"
+            width={700}
+            height={800}
+          />
         </div>
       </div>
       {children}
