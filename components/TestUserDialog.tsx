e4d97601d15bc78f85ecbd5d9051c8c8325fd12c
cezene <cezenesn@hotmail.com.br>
2025-07-06 14:01:43 -0300

    fixes test user dialog in mobile

--- components/TestUserDialog.tsx
+++ components/TestUserDialog.tsx
@@ -14,19 +14,19 @@ const TestUserDialog = () => {
         <DialogTrigger className="sidebar-link font-semibold text-yellow-200 cursor-pointer">
           Test User Credentials
         </DialogTrigger>
-        <DialogContent className="!bg-stone-900 !h-[300px] text-slate-50 border-slate-50 !gap-2 !px-10 flex  items-center justify-center">
+        <DialogContent className="!bg-stone-900 !h-[300px] text-slate-50 border-slate-50 !gap-2 flex  items-center justify-center !px-2 md:!px-10">
           <Image
             src="/icons/snoopy-credentials.png"
-            className="rounded-xl !ml-10"
+            className="rounded-xl !ml-5 md:!ml-10"
             alt="Snoopy Credentials Image"
             width={200}
             height={300}
           />
           <DialogHeader>
-            <DialogTitle className="font-semibold text-yellow-200 !ml-5">
+            <DialogTitle className="font-semibold text-yellow-200 md:!ml-5">
               Plaid Test User Credentials
             </DialogTitle>
-            <DialogDescription className="!ml-5">
+            <DialogDescription className="md:!ml-5">
               <div className="text16 font-normal text-yellow-200">
                 <p>User: user_good</p>
                 <p>Password: pass_good</p>
